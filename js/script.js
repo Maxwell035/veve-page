971b2e5f5ac1e84165d907e9dae52642cda71af5
DevOs <oscarbenitez198529@gmail.com>
2022-08-04 15:17:36 -0500

    página terminada

--- js/script.js
+++ js/script.js
@@ -9,3 +9,10 @@ window.onscroll= function(){
     }
 }
 
+const activeFoto= document.querySelector(".carousel-item");
+activeFoto.addEventListener("click", Active());
+
+function Active(){
+    activeFoto.classList.remove("active");
+    activeFoto.classList.add("active");
+}
